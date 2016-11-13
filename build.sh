89571e38bce8d62c936687a4ef705aed2608150b
Daniel Rasmuson <dan123911@gmail.com>
2016-11-13 13:31:55 -0800

    more starter code

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 mkdir -p dist
-./node_modules/rollup/bin/rollup src/bootstrap.js -o dist/bootstrap.js --format=cjs
+./node_modules/rollup/bin/rollup src/bootstrap.js -o dist/bundle.js --format=cjs
 ./node_modules/rollup/bin/rollup src/bootstrap-tests.js -o dist/bootstrap-tests.js --format=cjs
+cp -r src/* dist/
